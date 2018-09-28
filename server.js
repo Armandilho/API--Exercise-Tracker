df657339c8aeac1e953978059ca94e8cd16ff4bb
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-09-28 12:16:26 -0300

    register and query implemented

--- server.js
+++ server.js
@@ -6,18 +6,44 @@ const mongoose = require("mongoose");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
+//Conection to localhost
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Conectou no localhost, na porta 3000!");
+});
+//******/
+
 //Mongoose conection
 mongoose.connect(
-  "mongodb://localhost:27017/bancoapi",
+  "mongodb://localhost/test",
   { useNewUrlParser: true }
 );
 //******/
 
-//Conection to localhost
-const port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Conectou no banco, na porta 3000!");
+//Creating Schema
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  name: String
 });
+
+const Names = mongoose.model("Url", User);
 //******/
 
 app.get("/", (req, res) => res.sendFile("public/index.html"));
+
+app.get("/query/:username", async (req, res) => {
+  const { username } = req.params;
+  const arrayquer = await Names.find({ name: username });
+  if (arrayquer.length != 0) {
+    res.json({ name: username });
+  } else {
+    res.json({ error: "name not found" });
+  }
+});
+
+app.post("/registro", (req, res) => {
+  const { username } = req.body;
+  Names.create({ name: username });
+  res.json({ name: username });
+});
