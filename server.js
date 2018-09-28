e2de2e4c0892c9c7705b0494c188816cbe0ab78b
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-09-28 11:19:46 -0300

    Public folder added

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+//Mongoose conection
+mongoose.connect(
+  "mongodb://localhost:27017/bancoapi",
+  { useNewUrlParser: true }
+);
+//******/
+
+//Conection to localhost
 const port = process.env.PORT || 3000;
-
 app.listen(port, function() {
   console.log("Conectou no banco, na porta 3000!");
 });
+//******/
 
-app.get("/", (req, res) => {
-  res.json({ teste: "entrada" });
-});
+app.get("/", (req, res) => res.sendFile("public/index.html"));
